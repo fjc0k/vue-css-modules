1a563336bcdab21a0cb2b1bd464c267072a36486
fjc0k <fjc0kb@gmail.com>
2018-04-02 17:53:25 +0800

    build

--- dist/vue-css-modules.es.js
+++ dist/vue-css-modules.es.js
@@ -3,35 +3,107 @@
  * (c) 2018-present fjc0k <fjc0kb@gmail.com>
  * Released under the MIT License.
  */
+function includes(arrayLike, element, fromIndex) {
+  if (fromIndex === void 0) {
+    fromIndex = 0;
+  }
+
+  for (var i = fromIndex, len = arrayLike.length; i < len; i++) {
+    if (arrayLike[i] === element) {
+      return true;
+    }
+  }
+
+  return false;
+}
+function isObject(value) {
+  return value !== null && typeof value === 'object';
+}
+function isFunction(value) {
+  return typeof value === 'function';
+}
+
+var cache = Object.create(null);
+var modifiers = ['@', ':'];
+var parseClassExpression = (function (expression) {
+  if (cache[expression]) return cache[expression];
+  var modifier;
+  var className;
+  var binding;
+
+  if (includes(expression, '=', 1)) {
+    // eg: disabled=isDisabled
+    modifier = '=';
+
+    var _expression$split = expression.split('=');
+
+    className = _expression$split[0];
+    binding = _expression$split[1];
+  } else if (includes(modifiers, expression[0])) {
+    // eg: @button :disabled
+    modifier = expression[0];
+    className = expression.substr(1);
+  } else {
+    // eg: icon
+    className = expression;
+  }
+
+  cache[expression] = {
+    modifier: modifier,
+    className: className,
+    binding: binding,
+    role: modifier === '@' && className
+  };
+  return cache[expression];
+});
+
 var INJECTED = '__CSSModules__';
 var INJECT_ATTR = 'styleName';
 
-var inject = function inject(h) {
+function createElement(_) {
   var _this = this;
 
-  for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    args[_key - 1] = arguments[_key];
+  var args = [].slice.call(arguments, 1); // for functional component
+
+  if (isFunction(_)) {
+    return createElement.bind(_, {
+      createElement: _,
+      styles: args[0]
+    });
   }
 
+  var h = _.createElement,
+      _$styles = _.styles,
+      styles = _$styles === void 0 ? this && this.$style || {} : _$styles;
   var data = args[1];
 
-  if (typeof data === 'object') {
-    data.staticClass || (data.staticClass = '');
+  if (isObject(data)) {
+    if (!data.staticClass) {
+      data.staticClass = '';
+    }
+
+    if (!data.attrs) {
+      data.attrs = {};
+    }
+
     var modules = data[INJECT_ATTR] || data.attrs[INJECT_ATTR] || '';
 
     if (modules.length) {
       (Array.isArray(modules) ? modules : [modules]).forEach(function (module) {
         if (!module || typeof module !== 'string') return;
         module.split(/\s+/g).forEach(function (classExpression) {
-          var _classExpression$spli = classExpression.split('='),
-              className = _classExpression$spli[0],
-              binding = _classExpression$spli[1];
+          var _parseClassExpression = parseClassExpression(classExpression),
+              className = _parseClassExpression.className,
+              binding = _parseClassExpression.binding,
+              role = _parseClassExpression.role;
 
-          var staticClass = !binding && className || _this[binding] && className;
-
-          if (staticClass) {
-            data.staticClass += " " + staticClass;
+          if ((binding ? _this[binding] : true) && styles[className]) {
+            data.staticClass += " " + styles[className];
             data.staticClass.trim();
+          }
+
+          if (role) {
+            data.attrs['data-role'] = role;
           }
         });
       });
@@ -39,14 +111,19 @@ var inject = function inject(h) {
   }
 
   return h.apply(this, args);
-};
+}
 
-var index = {
-  beforeCreate: function beforeCreate() {
-    if (this[INJECTED]) return;
-    this[INJECTED] = true;
-    this.$createElement = inject.bind(this, this.$createElement);
-  }
-};
+var index = (function (styles) {
+  return {
+    beforeCreate: function beforeCreate() {
+      if (this[INJECTED]) return;
+      this[INJECTED] = true;
+      this.$createElement = createElement.bind(this, {
+        createElement: this.$createElement,
+        styles: styles
+      });
+    }
+  };
+});
 
 export default index;
